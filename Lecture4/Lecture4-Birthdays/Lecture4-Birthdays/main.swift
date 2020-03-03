0cfa5db02421beedff2e90b28c25cdfcffbfc4f3
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-03-03 08:29:56 +0100

    First commit Birthdays example

---
+++ Lecture4/Lecture4-Birthdays/Lecture4-Birthdays/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Lecture4-Birthdays
+//
+//  Created by Juan Carlos Ospina Gonzalez on 03/03/2020.
+//
+
+import Darwin
+import Python
+print(Python.version) //  Sanity check, prints 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53)
+let math = Python.import("math")
+
+
