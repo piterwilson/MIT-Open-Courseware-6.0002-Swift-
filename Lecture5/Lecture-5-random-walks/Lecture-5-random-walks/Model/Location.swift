a44ee145a3db5c47ffb0386b4fd5b1a1c96fe5e1
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-03-19 07:23:45 +0100

    Adds `Location` class

---
+++ Lecture5/Lecture-5-random-walks/Lecture-5-random-walks/Model/Location.swift
@@ -0,0 +1,46 @@
+//
+//  Location.swift
+//  Lecture-5-random-walks
+//
+//  Created by Juan Carlos Ospina Gonzalez on 12/03/2020.
+//
+
+import Foundation
+/*
+ class Location(object):
+ def __init__(self, x, y):
+     """x and y are numbers"""
+     self.x = x
+     self.y = y
+
+ def move(self, deltaX, deltaY):
+     """deltaX and deltaY are numbers"""
+     return Location(self.x + deltaX, self.y + deltaY)
+
+ def getX(self):
+     return self.x
+
+ def getY(self):
+     return self.y
+
+ def distFrom(self, other):
+     xDist = self.x - other.getX()
+     yDist = self.y - other.getY()
+     return (xDist**2 + yDist**2)**0.5
+
+ def __str__(self):
+     return '<' + str(self.x) + ', ' + str(self.y) + '>'
+ */
+
+struct Location {
+    var x: Double
+    var y: Double
+    func move(deltaX: Double, deltaY: Double) -> Location {
+        return Location(x: x + deltaX, y: y + deltaY)
+    }
+    func distFrom(_ location: Location) -> Double {
+        let xDist = x - location.x
+        let yDist = y - location.y
+        return (xDist ** 2 + xDist ** 2) ** 0.5
+    }
+}
