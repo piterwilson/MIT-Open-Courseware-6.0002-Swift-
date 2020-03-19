805e1549c0c9671c47637e463700dd91f0b6c079
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-03-19 07:21:48 +0100

    Starts Lecture5 example

---
+++ Lecture5/Lecture-5-random-walks/Lecture-5-random-walks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lecture-5-random-walks
+//
+//  Created by Juan Carlos Ospina Gonzalez on 12/03/2020.
+//  Copyright © 2020 Juan Carlos Ospina Gonzalez. All rights reserved.
+//
+
+import Foundation
+import Python
+print(Python.version) //  Sanity check, prints 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53)
+let random = Python.import("random")
