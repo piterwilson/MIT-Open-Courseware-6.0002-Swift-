5e429a6a6b28dca130ca9f1ce8df0b67ab870845
Juan Carlos Ospina Gonzalez <juan@twcapps.com>
2020-04-07 06:17:08 +0200

    Starts Lecture 6 example

---
+++ Lecture6/Lecture6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lecture6
+//
+//  Created by Juan Carlos Ospina Gonzalez on 07/04/2020.
+//  Copyright © 2020 piterwilsom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
